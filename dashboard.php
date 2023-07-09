fb248c65c6afc86b833a95e7ef706f2782f302fe
lamamanik <luffymonkz13@gmail.com>
2023-07-09 18:32:18 +0545

    wooo
    idk k ho yo

--- dashboard.php
+++ dashboard.php
@@ -1,113 +1,79 @@
+<?php
+include('config/db.php');
+
+$query = "SELECT * FROM users";
+
+$result = mysqli_query($conn, $query);
+
+$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+?>
+
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styledash.css" />
-    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="responsive.css" />
     <!-- Link Swiper's CSS -->
     <link rel="stylesheet" href="swiper/swiper-bundle.min.css" />
 
     <style>
-
-     .swiper-wrapper {
-        margin-left: 0 !important;  /*if it works no touching   */
+      .swiper-wrapper {
+        margin-left: 15px !important;
       }
     </style>
 
     <title>Dashbord</title>
   </head>
+
   <body>
     <?php
-      include('inc/header.php');
-    ?>
+    include('inc/header.php');
+  ?>
 
     <div class="container">
       <div class="swiper mySwiper1">
         <div class="slide-container swiper-wrapper">
+          <?php foreach ($users as $user): ?>
           <div class="card swiper-slide">
             <div class="employee">
-              <img
-                class="e-logo"
-                src="images/profile/p6.jpg"
-                alt="Employee Image"
-              />
+              <img class="e-logo" src="uploads/<?php echo $user["profilepic"]; ?>"
+              alt="Employee Image" />
               <div class="e-text">
-                <h2 class="employee-name">Manik lama</h2>
-                <h6 class="employee-post">Marketing</h6>
+                <h2 class="employee-name">
+                  <?php echo $user["firstname"] . " " . $user["lastname"]; ?>
+                </h2>
+                <h6 class="employee-post">
+                  <?php
+              $role = $user["role"];
+              switch ($role) {
+                case 1:
+                  echo "Admin";
+                  break;
+                case 2:
+                  echo "Manager";
+                  break;
+                case 3:
+                  echo "Staff";
+                  break;
+                default:
+                  echo "Unknown";
+                  break;
+              }
+              ?>
+                </h6>
               </div>
             </div>
             <div class="e-description">
               <p>
-                Lorem ipsum dolor sit amet consectetur. hello my name is manik
-                lama currently doing bachelor in southwestern stsate clz
+                <?php echo $user["description"]; ?>
               </p>
             </div>
           </div>
-
-          <div class="card swiper-slide">
-                <div class="employee">
-                  <img
-                    class="e-logo"
-                    src="images/profile/p2.jpg"
-                    alt="Employee Image"
-                  />
-                  <div class="e-text">
-                    <h2 class="employee-name">Manik lama</h2>
-                    <h6 class="employee-post">Ui designer</h6>
-                  </div>
-                </div>
-                <div class="e-description">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur. Lorem ipsum, dolor
-                    sit amet consectetur adipisicing elit.
-                  </p>
-                </div>
-              </div>
-
-
-              <div class="card swiper-slide">
-                <div class="employee">
-                  <img
-                    class="e-logo"
-                    src="images/profile/p2.jpg"
-                    alt="Employee Image"
-                  />
-                  <div class="e-text">
-                    <h2 class="employee-name">Manik lama</h2>
-                    <h6 class="employee-post">Ui designer</h6>
-                  </div>
-                </div>
-                <div class="e-description">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur. Lorem ipsum, dolor
-                    sit amet consectetur adipisicing elit.
-                  </p>
-                </div>
-              </div>
-
-              <div class="card swiper-slide">
-                <div class="employee">
-                  <img
-                    class="e-logo"
-                    src="images/profile/p2.jpg"
-                    alt="Employee Image"
-                  />
-                  <div class="e-text">
-                    <h2 class="employee-name">Manik lama</h2>
-                    <h6 class="employee-post">Ui designer</h6>
-                  </div>
-                </div>
-                <div class="e-description">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur. Lorem ipsum, dolor
-                    sit amet consectetur adipisicing elit.
-                  </p>
-                </div>
-              </div>
-
-
-
+          <?php endforeach; ?>
         </div>
       </div>
     </div>
