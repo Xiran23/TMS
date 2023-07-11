a398fa2cf3cc045833561513c47b8b3987939a7a
Xiran23 <thapachiran23@gmail.com>
2023-07-12 00:29:02 +0545

    sdf

--- inc/header.php
+++ inc/header.php
@@ -63,7 +63,7 @@ if(isset($_POST['logout'])){
           <li>
             <a href="dashboard.php">
               <img class="dashlogo" src="images/icon/dashboard.png" alt="" />
-              <span class="nav-item">DASHBOARD2</span>
+              <span class="nav-item">DASHBOARD</span>
             </a>
           </li>
           <!-- creating TASKS -->
