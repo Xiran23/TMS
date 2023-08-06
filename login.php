4e341e1a908604f761ac465335d020dec421bf48
MYnik69 <91261883+MYnik69@users.noreply.github.com>
2023-08-06 21:02:10 +0545

    favicon
    favicon

--- login.php
+++ login.php
@@ -75,6 +75,8 @@ if (!isset($_SESSION['username'])) {
 
 <head>
   <meta charset="UTF-8" />
+  <link rel="icon" href="images/picture2.png" type="image/png">
+  <link rel="shortcut icon" href="images/picture2.png" type="image/png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="style.css" />
